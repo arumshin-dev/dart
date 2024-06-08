407943f3c551bee44fff154d9a6fe55147548c03
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-06-08 10:35:24 +0000

    variables

--- variables.dart
+++ variables.dart
@@ -10,4 +10,20 @@ void main(){
 
   const birthday = 0323;
   print(birthday);
+
+  var x;
+  x=1;
+  int i = 12;
+  x='x';
+  dynamic anything;
+  anything = 'String';
+  anything = 1;
+  anything = true;
+
+  String? temp1 = 'temp1';
+  temp1 = null;
+  temp1?.isEmpty;
+  print(temp1);
+  print(temp1?.isEmpty);
+  
 }
